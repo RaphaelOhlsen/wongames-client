82c8a825a911d5bdc0cb2f99fefa52c9e721797c
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-09-09 09:08:36 -0300

    Logo Parte 5: Criando primeiros testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render a whaite label by default, () => {
-    
+  it('should render a white label by default', () => {
+    // render component
+    // select element to be test `screen` (queries) - getByLabel
+    //expect - compare, analyze
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })

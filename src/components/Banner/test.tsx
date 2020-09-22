4284e840504dd907dfee53207dbab93665bce82e
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-09-22 10:53:35 -0300

    Criando a estrutura do Banner e escrevendo testes

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willanjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render corretly', () => {
+    renderWithTheme(<Banner {...props}></Banner>)
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /play the new CrashLands season/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+  })
+})

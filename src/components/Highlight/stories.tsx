cbe30fa41257690e96ef9798645cef908c285fdf
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-09-25 13:08:38 -0300

    Criando o CSS do componente Highlights

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -3,13 +3,21 @@ import Highlight, { HighlightProps } from '.'
 
 export default {
   title: 'Highlight',
-  component: Highlight
+  component: Highlight,
+  args: {
+    title: 'Read Dead está de volta!',
+    subtitle: 'Venha conhecer as novas aventuas de John Marston',
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
-Default.args = {
-  title: 'Read Dead está de volta!',
-  subtitle: 'Venha conhecer as novas aventuas de John Marston',
-  buttonLabel: 'Buy now',
-  buttonLink: '/rdr2'
-}
+// Default.args = {
+//   title: 'Read Dead está de volta!',
+//   subtitle: 'Venha conhecer as novas aventuas de John Marston',
+//   backgroundImage: '/img/red-dead-img.jpg',
+//   buttonLabel: 'Buy now',
+//   buttonLink: '/rdr2'
+// }
--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,12 +1,55 @@
+import media from 'styled-media-query'
 import styled, { css } from 'styled-components'
+import { HighlightProps } from '.'
 
-export const Wrapper = styled.div``
+type WrapperProps = Pick<HighlightProps, 'backgroundImage'>
+
+export const Wrapper = styled.section<WrapperProps>`
+  ${({ backgroundImage }) => css`
+    position: relative;
+    height: 23rem;
+    display: grid;
+    background-image: url(${backgroundImage});
+    background-position: center center;
+    background-size: cover;
+
+    ${media.greaterThan('medium')`
+      height: 32rem;
+    `}
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.6);
+    }
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    z-index: ${theme.layers.base};
+    text-align: right;
+    padding: ${theme.spacings.xsmall};
+
+    ${media.greaterThan('medium')`
+      align-self: end;
+      padding: ${theme.spacings.large};
+    `}
+  `}
+`
 
 export const Title = styled.h2`
   ${({ theme }) => css`
-    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.large};
     font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+    margin-bottom: ${theme.spacings.xxsmall};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge}
+    `}
   `}
 `
 export const Subtitle = styled.h3`
@@ -14,5 +57,10 @@ export const Subtitle = styled.h3`
     color: ${theme.colors.white};
     font-size: ${theme.font.sizes.small};
     font-weight: ${theme.font.light};
+    margin-bottom: ${theme.spacings.medium};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.large}
+    `}
   `}
 `
